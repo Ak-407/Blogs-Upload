56fa0a685063e68120a7e3abceb436ba4ffc248a
Syed Amaan <75304633+Ak-407@users.noreply.github.com>
2023-01-21 14:21:10 +0530

    second

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ mongoose.set('strictQuery', true);
 mongoose.connect("mongodb+srv://jsamaan:amaan123@cluster0.vz55wc0.mongodb.net/jsamaan?retryWrites=true&w=majority",{ useNewUrlParser: true , useUnifiedTopology: true });
 const app=express();
 const myhome="Hey, this is my homepage, so I have to say something about myself. Sometimes it is hard to introduce yourself because you know yourself so well that you do not know where to start with. Let me give a try to see what kind of image you have about me through my self-description. I hope that my impression about myself and your impression about me are not so different. Here it goes.";
-const myselfbreif="I am a person who is positive about every aspect of life. There are many things I like to do, to see, and to experience. I like to read, I like to write; I like to think, I like to dream; I like to talk, I like to listen. I like to see the sunrise in the morning, I like to see the moonlight at night; I like to feel the music flowing on my face, I like to smell the wind coming from the ocean. I like to look at the clouds in the sky with a blank mind, I like to do thought experiment when I cannot sleep in the middle of the night. I like flowers in spring, rain in summer, leaves in autumn, and snow in winter. I like to sleep early, I like to get up late; I like to be alone, I like to be surrounded by people. I like country’s peace, I like metropolis’ noise; I like the beautiful west lake in Hangzhou, I like the flat cornfield in Champaign. I like delicious food and comfortable shoes; I like good books and romantic movies. I like the land and the nature, I like people. And, I like to laugh.";
+const myselfbreif="I am a person who is positive about every aspect of life. There are many things I like to do, to see, and to experience. I like to read, I like to write; I like to think, I like to dream; I like to talk, I like to listen. I like to see the sunrise in the morning, I like to see the moonlight at night; I like to feel the music flowing on my face, I like to smell the wind coming from the ocean. I like to look at the clouds in the sky with a blank mind, I like to do thought experiment when I cannot sleep in the middle of the night. I like flowers in spring, rain in summer, leaves in autumn, and snow in winter. I like to sleep early, I like to get up late; I like to be alone, I like to be surrounded by people. I like countrys peace, I like metropolis noise; I like the beautiful west lake in Hangzhou, I like the flat cornfield in Champaign. I like delicious food and comfortable shoes; I like good books and romantic movies. I like the land and the nature, I like people. And, I like to laugh.";
 const mycontact="6006466047 is my own number you can call me or email me on given Gmail ID-- amaaniqbal3k@gmail.com";
 
 
@@ -37,10 +37,10 @@ app.get("/", function(req,res){
 
 });
 app.get("/about", function(req,res){
-    res.render("list", {home:"About Me",mybreif:myselfbreif, composehead:"",composeinf:""});
+    res.render("about", {mybreif:myselfbreif});
 });
 app.get("/contact", function(req,res){
-    res.render("list", {home:"Contact Me",mybreif:mycontact, composehead:"",composeinf:""});
+    res.render("contact", {mybreif:mycontact});
 });
 app.get("/compose", function(req,res){
     res.render("compose");
@@ -76,7 +76,7 @@ app.get("/:upper", function(req,res){
 
 
 
-app.listen("3000", function(req, res){
+app.listen("300", function(req, res){
     console.log("Port 3000 is running");
 });
 
